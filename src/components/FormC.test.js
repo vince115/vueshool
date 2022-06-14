3868934f662c4ccb0a075e4cabcdfc115dfa5716
Vince Lo <vince115@gmail.com>
2022-06-14 10:15:10 +0800

    refine FormC component test

--- src/components/FormC.test.js
+++ src/components/FormC.test.js
@@ -4,20 +4,16 @@
  import { describe, it, expect } from 'vitest'
  import { mount } from '@vue/test-utils'
  import FormC from '../components/FormC.vue'
- //import FormC from '../components/Counter.vue'
 
  describe('FormC', () => {
-  // it('is okay', async () => {
-  //   const wrapper = mount(FormC, {})
-  //   await wrapper.find('input[type=email]').setValue(email)  // })
-    it('submits a form' , () => {
-      
+    it('submits a form' , async() => {  
         const wrapper = mount(FormC)
-        // const email = 'name@mail.com'
-        // await wrapper.find('input[type=email]').setValue(email)
-        // await wrapper.find('textarea').setValue('Lorem ipsum dolor sit amet')
-        // await wrapper.find('select').setValue('moscow')
-        // await wrapper.find('input[type=checkbox]').setValue()
-        // await wrapper.find('input[type=radio][value=monthly]').setValue()
+        const email = 'name@mail.com'
+        await wrapper.find('input[type=email]').setValue(email)
+        await wrapper.find('textarea').setValue('Lorem ipsum dolor sit amet')
+        await wrapper.find('select').setValue('moscow')
+        await wrapper.find('input[type=checkbox]').setValue()
+        await wrapper.find('input[type=radio]').setValue()
+        await wrapper.find("input[type=radio][value='monthly']").setValue()
       })
   })
